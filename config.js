350522819ea28d6cbf637f17e1ed309012bd8620
Tadashi0000001 <imeshtadashi@gmail.com>
2025-06-22 23:24:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 module.exports = {
   DATABASE_URL: 'postgresql://de_ter_user:594W0ioJ2KRQ4YIkVOQVAZBcdey3lFrg@dpg-d14pqkq4d50c73cjoa40-a.oregon-postgres.render.com/de_ter',
   PORT: 9090,
-  OWNER_NUMBER: ['94789958225'],
+  OWNER_NUMBER: ['94743370472'],
   IMGBB_API_KEY: '3839e303da7b555ec5d574e53eb836d2',
   RENDER_JSON_URL: '',
   AUTO_STATUS_SEEN: true,

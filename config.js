13c47ed847b0084260704ec9bb714d644cd3c765
Tadashi0000001 <imeshtadashi@gmail.com>
2025-06-27 07:49:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // DEXTER POWER BY CODE
 module.exports = {
-  DATABASE_URL: 'postgresql://de_ter_user:594W0ioJ2KRQ4YIkVOQVAZBcdey3lFrg@dpg-d14pqkq4d50c73cjoa40-a.oregon-postgres.render.com/de_ter',
+  DATABASE_URL: 'postgresql://new_database_song_user:U8nZEM8jjpyy50MhkM87vJgHPYgBGaBB@dpg-d1cvptruibrs73dm6dcg-a/new_database_song',
   PORT: 9090,
   OWNER_NUMBER: ['94743370472'],
   IMGBB_API_KEY: '3839e303da7b555ec5d574e53eb836d2',
